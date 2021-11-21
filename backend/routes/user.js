d9d3be6852c98098643a87248fb57f4511964c5a
Agam Singh <agamsingh.it@gmail.com>
2021-11-21 21:42:15 +1100

    seperated valid files

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,32 +1,24 @@
 // PACKAGES
 const router = require("express").Router();
-const Joi = require("joi");
 // IMPORT FILES
 const User = require("../models/user");
-
-// validation
-
-const schema = Joi.object({
-  name: Joi.string().min(3).required(),
-  email: Joi.string().min(6).required().email(),
-  password: Joi.string().min(6).required(),
-});
-
+const { signUpValid, signInValid } = require("../validation");
 // ROUTES
 router.post("/signup", async (req, res) => {
-  const { error } = schema.validate(req.body);
-  res.send(error.details[0].message);
-  //   const user = new User({
-  //     name: req.body.name,
-  //     email: req.body.email,
-  //     password: req.body.password,
-  //   });
-  //   try {
-  //     const savedUser = await user.save();
-  //     res.send(savedUser);
-  //   } catch (err) {
-  //     res.status(404).send(err);
-  //   }
+  const { error } = signUpValid(req.body);
+  if (error) return res.status(404).send(error.details[0].message);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(404).send(err);
+  }
 });
 
 module.exports = router;
