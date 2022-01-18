830cb8a5a4828a9994c816808965da6ad8638272
Agam Singh <agamsingh.it@gmail.com>
2022-01-19 08:59:23 +1100

    nav 2 change

--- frontend/fam-friends-app/src/app/components/nav2/nav2.component.ts
+++ frontend/fam-friends-app/src/app/components/nav2/nav2.component.ts
@@ -5,7 +5,7 @@ import { LoginService } from 'src/app/services/login.service';
 @Component({
   selector: 'app-nav2',
   templateUrl: './nav2.component.html',
-  styleUrls: ['./nav2.component.css'],
+  styleUrls: ['./nav2.component.scss'],
 })
 export class Nav2Component implements OnInit {
   constructor(private router: Router, private loginService: LoginService) {}
